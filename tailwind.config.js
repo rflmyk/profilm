402a23c7926326a2b06a87b7f6b25cea4a1657cf
Mayrink <rflmyk@gmail.com>
2023-07-05 09:50:59 -0300

    apply svgs and dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/xtendui/src/*.mjs",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/xtendui/src/*.mjs"],
   theme: {
     extend: {},
   },
   plugins: [],
   presets: [require("xtendui/tailwind.preset")],
+  darkMode: "class",
 };
